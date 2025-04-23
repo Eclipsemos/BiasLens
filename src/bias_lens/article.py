15de1e4eefd56b0d5437d18cf9ff042c73ca3b46
yucong3 <yucong3@illinois.edu>
2025-04-22 20:10:43 -0500

    backend WIP
    backend WIP

---
+++ src/bias_lens/article.py
@@ -0,0 +1,14 @@
+
+
+class Article:
+
+    def __init__(self):
+        
+        self.page_content = ""
+        self.title = ""
+        self.authors = []
+        self.institution = ""
+        self.category = ""
+        self.time = ""
+        self.abstract = ""
+        self.entities = []
